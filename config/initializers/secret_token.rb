5d59f43cf0862f4ad9f0b3ea18c8c240406ebfab
George Petrov <jokopetrov@gmail.com>
2014-05-27 12:51:56 +0300

    Trevor Web V1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trevor::Application.config.secret_token = '23b98f19965ec515f171e2881e337561f58b732e45b5c778611cf54b0ad42c84d36f122e38bc1a43f036cafb459203cb7f0c36bbf937aa875580b9e07af03212'
